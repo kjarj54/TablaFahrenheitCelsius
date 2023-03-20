b879f5699b752a6181106684d5261de22360e6ad
Arauz <86898345+kjarj54@users.noreply.github.com>
2023-03-20 13:46:07 -0600

    a

--- ejercicioTarea1.cpp
+++ ejercicioTarea1.cpp
@@ -10,7 +10,7 @@ int main(){
 	double fahrenheit, celsius, temp;
 	int MENOR = 0;
 	int MAYOR = 300;
-	int PASOS = 20;
+	int INCR = 20;
 	char r;
 	
 	do{
@@ -20,7 +20,7 @@ int main(){
 		cout<<setw(10)<<"Fahrenheit"<<setw(15)<<"Celsius"<<endl;//Setea los encabezados en un pocicion especifica
 		cout<<"--------------------------------------------"<<endl;
 			
-		for (fahrenheit = temp; fahrenheit <= MAYOR; fahrenheit += PASOS) {
+		for (fahrenheit = temp; fahrenheit <= MAYOR; fahrenheit += INCR) {
         	celsius = (5.0 / 9.0) * (fahrenheit - 32.0);
         	cout << fixed << setprecision(2) << setw(10) << fahrenheit << setw(15) << celsius << endl;//Setea las conversiones en la tabla y una posicion especifica
     	}
